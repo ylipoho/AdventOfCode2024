96ed233fb2343b274a6ce18c942e08ac0a503df1
Baranouskaya Lizaveta <elizaveta.baranovskaya.21@gmail.com>
2024-12-02 18:51:28 +0300

    Added solution for Day2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace AdventOfCode2024
 		{
 			Console.WriteLine($"1.1: {Day1.GetDistancesSum_v1()}");
 			Console.WriteLine($"1.2: {Day1.GetSimilarityScoresSum_v2()}");
+			Console.WriteLine($"2.1: {Day2.GetSafeReportsNum_v1()}");
+			Console.WriteLine($"2.2: {Day2.GetSafeReportsNum_v2()}");
 		}
 	}
 }
---
+++ src/Day2.cs
@@ -0,0 +1,96 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+namespace AdventOfCode2024.src
+{
+	internal class Day2
+	{
+		public static int GetSafeReportsNum_v1()
+		{
+			var lines = FileReader
+							.ReadFile("2")
+							.Select(l => l
+										.Split(' ')
+										.Select(n => int.Parse(n))
+										.ToList());
+
+			return lines.Count(l => AreConditionsMet(l));
+		}
+
+		public static int GetSafeReportsNum_v2()
+		{
+			var lines = FileReader
+							.ReadFile("2")
+							.Select(l => l
+										.Split(' ')
+										.Select(n => int.Parse(n))
+										.ToList());
+
+			int sum = 0;
+			
+			foreach (var line in lines)
+			{
+				int index = -1;
+
+				while (index < line.Count)
+				{
+					List<int> line2 = new(line);
+
+					if (index != -1)
+					{
+						line2.RemoveAt(index);
+					}
+
+					if (AreConditionsMet(line2))
+					{
+						sum++;
+						break;
+					}
+
+					index++;
+				}
+			}
+
+			return sum;
+		}
+
+		static bool AreConditionsMet(List<int> line) => (IsLevelIncreasing(line) || IsLevelDecreasing(line))
+															&& AreDifferencesAcceptable(line);
+
+		static bool IsLevelIncreasing(List<int> line)
+		{
+			for (int i = 0; i < line.Count - 1; i++)
+			{
+				if (line[i] >= line[i + 1])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		static bool IsLevelDecreasing(List<int> line)
+		{
+			for (int i = 0; i < line.Count - 1; i++)
+			{
+				if (line[i] <= line[i + 1])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		static bool AreDifferencesAcceptable(List<int> line)
+		{
+			for (int i = 0; i < line.Count - 1; i++)
+			{
+				if (Math.Abs(line[i] - line[i + 1]) > 3)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
