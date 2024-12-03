d06b2b2d4c108fc074e64fd1708b34662c3ea5e5
Baranouskaya Lizaveta <elizaveta.baranovskaya.21@gmail.com>
2024-12-03 18:51:31 +0300

    Add solution for Day3

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace AdventOfCode2024
 			Console.WriteLine($"1.2: {Day1.GetSimilarityScoresSum_v2()}");
 			Console.WriteLine($"2.1: {Day2.GetSafeReportsNum_v1()}");
 			Console.WriteLine($"2.2: {Day2.GetSafeReportsNum_v2()}");
+			Console.WriteLine($"3.1: {Day3.GetMultiplicationsSum_v1()}");
+			Console.WriteLine($"3.2: {Day3.GetMultiplicationsSum_v2()}");
 		}
 	}
 }
