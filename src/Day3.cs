8b2079fbe83c113865b6fae7b1a239247b1dc3ab
Baranouskaya Lizaveta <elizaveta.baranovskaya.21@gmail.com>
2024-12-03 23:07:40 +0300

    edit

--- src/Day3.cs
+++ src/Day3.cs
@@ -2,46 +2,46 @@
 
 namespace AdventOfCode2024.src
 {
-	internal class Day3
-	{
-		public static int GetMultiplicationsSum_v1()
-		{
-			return FileReader.ReadFile("3")
-							.Select(line => Regex
-											.Matches(line, @"(?<=mul\()\d+,\d+(?=\))")
-											.Select(m => m.Value
-															.Split(',')
-															.Select(n => int.Parse(n))
-															.Aggregate((x, y) => x * y))
-											.Sum())
-							.Sum();
-		}
+    internal class Day3
+    {
+        public static int GetMultiplicationsSum_v1()
+        {
+            return FileReader.ReadFile("3")
+                            .Select(line => Regex
+                                            .Matches(line, @"(?<=mul\()\d+,\d+(?=\))")
+                                            .Select(m => m.Value
+                                                            .Split(',')
+                                                            .Select(n => int.Parse(n))
+                                                            .Aggregate((x, y) => x * y))
+                                            .Sum())
+                            .Sum();
+        }
 
-		public static int GetMultiplicationsSum_v2()
-		{
-			string line = string.Join(string.Empty, FileReader.ReadFile("3"));
-			var matches = Regex.Matches(line, @"(?<=mul\()\d+,\d+(?=\))|do\(\)|don't\(\)");
+        public static int GetMultiplicationsSum_v2()
+        {
+            string line = string.Join(string.Empty, FileReader.ReadFile("3"));
+            var matches = Regex.Matches(line, @"(?<=mul\()\d+,\d+(?=\))|do\(\)|don't\(\)");
 
-			int sum = 0;
-			bool isAvailable = true;
+            int sum = 0;
+            bool isAvailable = true;
 
-			foreach (Match match in matches.Cast<Match>())
-			{
+            foreach (Match match in matches.Cast<Match>())
+            {
                 if (match.Value == "do()")
                 {
-					isAvailable = true;
+                    isAvailable = true;
                 }
-				else if (match.Value == "don't()")
-				{
-					isAvailable = false;
-				}
-				else if (isAvailable)
-				{
-					var numbers = match.Value.Split(',');
-					sum += int.Parse(numbers[0]) * int.Parse(numbers[1]);
-				}
-			}
-			return sum;
-		}
-	}
+                else if (match.Value == "don't()")
+                {
+                    isAvailable = false;
+                }
+                else if (isAvailable)
+                {
+                    var numbers = match.Value.Split(',');
+                    sum += int.Parse(numbers[0]) * int.Parse(numbers[1]);
+                }
+            }
+            return sum;
+        }
+    }
 }
