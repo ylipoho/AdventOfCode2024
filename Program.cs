c957a1f7171245e24f4409d9ffc5a23e96f22a7a
Baranouskaya Lizaveta <elizaveta.baranovskaya.21@gmail.com>
2024-12-13 04:37:47 +0300

    Add solution for Day10 + add spaces

--- Program.cs
+++ Program.cs
@@ -6,18 +6,20 @@ namespace AdventOfCode2024
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"1.1: {Day1.GetDistancesSum_v1() == 2196996}");
-            Console.WriteLine($"1.2: {Day1.GetSimilarityScoresSum_v2() == 23655822}");
-            Console.WriteLine($"2.1: {Day2.GetSafeReportsNum_v1() == 585}");
-            Console.WriteLine($"2.2: {Day2.GetSafeReportsNum_v2() == 626}");
-            Console.WriteLine($"3.1: {Day3.GetMultiplicationsSum_v1() == 183788984}");
-            Console.WriteLine($"3.2: {Day3.GetMultiplicationsSum_v2() == 62098619}");
-            Console.WriteLine($"7.1: {Day7.GetTotalCalibrationResult(taskPart: 1) == 1430271835320}");
-            Console.WriteLine($"7.2: {Day7.GetTotalCalibrationResult(taskPart: 2) == 456565678667482}");
-            Console.WriteLine($"8.1: {Day8.GetAntinodeLocationsCount(taskPart: 1) == 311}");
-            Console.WriteLine($"8.2: {Day8.GetAntinodeLocationsCount(taskPart: 2) == 1115}");
-            Console.WriteLine($"9.1: {Day9.GetFilesystemChecksum(taskPart: 1) == 6258319840548}");
-            Console.WriteLine($"9.2: {Day9.GetFilesystemChecksum(taskPart: 2) == 6286182965311}");
+            Console.WriteLine($" 1.1: {Day1.GetDistancesSum_v1() == 2196996}");
+            Console.WriteLine($" 1.2: {Day1.GetSimilarityScoresSum_v2() == 23655822}");
+            Console.WriteLine($" 2.1: {Day2.GetSafeReportsNum_v1() == 585}");
+            Console.WriteLine($" 2.2: {Day2.GetSafeReportsNum_v2() == 626}");
+            Console.WriteLine($" 3.1: {Day3.GetMultiplicationsSum_v1() == 183788984}");
+            Console.WriteLine($" 3.2: {Day3.GetMultiplicationsSum_v2() == 62098619}");
+            Console.WriteLine($" 7.1: {Day7.GetTotalCalibrationResult(taskPart: 1) == 1430271835320}");
+            Console.WriteLine($" 7.2: {Day7.GetTotalCalibrationResult(taskPart: 2) == 456565678667482}");
+            Console.WriteLine($" 8.1: {Day8.GetAntinodeLocationsCount(taskPart: 1) == 311}");
+            Console.WriteLine($" 8.2: {Day8.GetAntinodeLocationsCount(taskPart: 2) == 1115}");
+            Console.WriteLine($" 9.1: {Day9.GetFilesystemChecksum(taskPart: 1) == 6258319840548}");
+            Console.WriteLine($" 9.2: {Day9.GetFilesystemChecksum(taskPart: 2) == 6286182965311}");
+            Console.WriteLine($"10.1: {Day10.GetTrailheadsScoreSum(taskPart: 1) == 501}");
+            Console.WriteLine($"10.2: {Day10.GetTrailheadsScoreSum(taskPart: 2) == 1017}");
         }
     }
 }
---
+++ src/Day10.cs
@@ -0,0 +1,82 @@
+﻿namespace AdventOfCode2024.src
+{
+    internal class Day10
+    {
+        static int[][] Map = [];
+
+        public static int GetTrailheadsScoreSum(int taskPart)
+        {
+            Map = FileReader
+                    .ReadLines("10")
+                    .Select(line => line
+                                    .Select(x => x - '0')
+                                    .ToArray())
+                    .ToArray();
+
+            List<List<(int X, int Y)>> ninePoints = [];
+
+
+            for (int i = 0; i < Map.Length; i++)
+            {
+                for (int j = 0; j < Map[i].Length; j++)
+                {
+                    if (Map[i][j] == 0)
+                    {
+                        ninePoints.Add(FindWayToNinePoint(i, j));
+                    }
+                }
+            }
+
+            return taskPart == 1
+                ? ninePoints.Select(x => x.GroupBy(x => x).Count()).Sum()
+                : ninePoints.Select(x => x.Count).Sum();
+        }
+
+        static List<(int, int)> FindWayToNinePoint(int x, int y)
+        {
+            if (Map[x][y] == 9)
+            {
+                return [(x, y)];
+            }
+
+            List<(int, int)> result = [];
+
+            foreach (var (X, Y) in FindNeighbours(x, y))
+            {
+                if (Map[x][y] + 1 == Map[X][Y])
+                {
+                    result.AddRange(FindWayToNinePoint(X, Y));
+                }
+            }
+
+            return result;
+        }
+
+        static List<(int X, int Y)> FindNeighbours(int x, int y)
+        {
+            List<(int X, int Y)> neighbours = [];
+
+            if (x > 0)
+            {
+                neighbours.Add((x - 1, y));
+            }
+
+            if (y > 0)
+            {
+                neighbours.Add((x, y - 1));
+            }
+
+            if (x < Map.Length - 1)
+            {
+                neighbours.Add((x + 1, y));
+            }
+
+            if (y < Map[0].Length - 1)
+            {
+                neighbours.Add((x, y + 1));
+            }
+
+            return neighbours;
+        }
+    }
+}
