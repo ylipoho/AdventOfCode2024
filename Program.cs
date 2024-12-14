d8a23e422fd350a21cfa78e990bbfb950fc59db0
Baranouskaya Lizaveta <elizaveta.baranovskaya.21@gmail.com>
2024-12-15 01:14:50 +0300

    Finally refactored Day4

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace AdventOfCode2024
             Console.WriteLine($" 2.2: {Day2.GetSafeReportsNum_v2() == 626}");
             Console.WriteLine($" 3.1: {Day3.GetMultiplicationsSum_v1() == 183788984}");
             Console.WriteLine($" 3.2: {Day3.GetMultiplicationsSum_v2() == 62098619}");
+            Console.WriteLine($" 4.1: {Day4.GetEntriesCount_v1() == 2549}");
+            Console.WriteLine($" 4.2: {Day4.GetEntriesCount_v2() == 2003}");
             Console.WriteLine($" 7.1: {Day7.GetTotalCalibrationResult(taskPart: 1) == 1430271835320}");
             Console.WriteLine($" 7.2: {Day7.GetTotalCalibrationResult(taskPart: 2) == 456565678667482}");
             Console.WriteLine($" 8.1: {Day8.GetAntinodeLocationsCount(taskPart: 1) == 311}");
---
+++ src/Day4.cs
@@ -0,0 +1,108 @@
+﻿namespace AdventOfCode2024.src
+{
+    internal class Day4
+    {
+        static string[] Map = [];
+
+        public static int GetEntriesCount_v1()
+        {
+            Map = FileReader
+                    .ReadLines("4")
+                    .ToArray();
+
+            int mapSize = Map.Length;
+            int count = 0;
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (Map[i][j] == 'X')
+                    {
+                        count += ProcessXCell(i, j);
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        static int ProcessXCell(int x, int y)
+        {
+            List<(int xCoeff, int yCoeff)> directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+            char[] letters = ['X', 'M', 'A', 'S'];
+            int count = 0;
+            int mapSize = Map.Length;
+
+            foreach (var (xCoeff, yCoeff) in directions)
+            {
+                int xmasIndex = 0;
+                int xLetter = x;
+                int yLetter = y;
+
+                do
+                {
+                    xmasIndex++;
+                    xLetter = x + Math.Sign(xCoeff) * Math.Abs(xmasIndex);
+                    yLetter = y + Math.Sign(yCoeff) * Math.Abs(xmasIndex);
+                } while (xLetter >= 0 && xLetter < mapSize
+                    && yLetter >= 0 && yLetter < mapSize
+                    && xmasIndex < 4 && Map[xLetter][yLetter] == letters[xmasIndex]);
+
+                if (xmasIndex == 4)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int GetEntriesCount_v2()
+        {
+            Map = FileReader
+                    .ReadLines("4")
+                    .ToArray();
+
+            int count = 0;
+
+            for (int i = 1; i < Map.Length - 1; i++)
+            {
+                 count += Map[i]
+                            .Select((letter, index) => (letter, index))
+                            .Count(cell => cell.letter == 'A' && IsXPattern(i, cell.index));
+            }
+
+            return count;
+        }
+
+        private static bool IsXPattern(int x, int y)
+        {
+            if (x == 0 || y == 0 || x >= Map.Length - 1 || y >= Map.Length - 1)
+            {
+                return false;
+            }
+
+            bool upM = Map[x - 1][y - 1] == 'M'
+                        && Map[x - 1][y + 1] == 'M'
+                        && Map[x + 1][y - 1] == 'S'
+                        && Map[x + 1][y + 1] == 'S';
+
+            bool downM = Map[x - 1][y - 1] == 'S'
+                        && Map[x - 1][y + 1] == 'S'
+                        && Map[x + 1][y - 1] == 'M'
+                        && Map[x + 1][y + 1] == 'M';
+
+            bool leftM = Map[x - 1][y - 1] == 'M'
+                        && Map[x - 1][y + 1] == 'S'
+                        && Map[x + 1][y - 1] == 'M'
+                        && Map[x + 1][y + 1] == 'S';
+
+            bool rightM = Map[x - 1][y - 1] == 'S'
+                        && Map[x - 1][y + 1] == 'M'
+                        && Map[x + 1][y - 1] == 'S'
+                        && Map[x + 1][y + 1] == 'M';
+
+            return upM || downM || leftM || rightM;
+        }
+    }
+}
